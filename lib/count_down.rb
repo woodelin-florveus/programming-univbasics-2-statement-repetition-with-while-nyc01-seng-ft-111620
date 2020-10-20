29ef5a8cb2194794fc58c22edca489e84d870ec2
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-20 19:33:02 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,8 +1,16 @@
 # Write your code here
 
+# count = 10
+# while count >= 1 do
+#   puts count
+#   count -= 1
+# end
+# puts "Happy New Year!"
+
 count = 10
-while count >= 1 do
-  puts count
-  count -= 1
+until count == 0
+  # puts "The current number is less than 20."
+  puts "#{count}"
+  counter -= 1
 end
-puts "Happy New Year!"
+puts "Happy New Years"
